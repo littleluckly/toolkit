79dc3ec7f914faf59b7ceb6423ebd7f48cf3c890
luck_body <707798472@qq.com>
2023-08-23 21:07:33 +0800

    同步函数增加递归写法

--- src/libs/compose/SyncCompose.ts
+++ src/libs/compose/SyncCompose.ts
@@ -1,4 +1,19 @@
 type Fn = (...args: any[]) => any
+
+export const syncCompose2 = (fns: Fn[]) => {
+  let i = 0
+  return function dispatch(...args: any[]): any {
+    return Promise.resolve(fns[i].apply(null, args)).then(result => {
+      i += 1
+      if (i < fns.length) {
+        return dispatch(result)
+      }
+      return Promise.resolve(result)
+    })
+  }
+}
+
+
 export const syncCompose = (fns: Fn[]) => {
   return function (...args: any[]) {
     return fns.reduce((prevePromise, currPromise) => {
@@ -6,3 +21,5 @@ export const syncCompose = (fns: Fn[]) => {
     }, Promise.resolve(fns.shift()?.apply(null, args)))
   }
 }
+
+
--- test/syncCompose.test.js
+++ test/syncCompose.test.js
@@ -28,4 +28,31 @@ describe("同步compose", () => {
     await composeFn(1);
     expect(result).toEqual([1, 2, 3]);
   });
+
+  it("函数报错", async () => {
+    const result = [];
+    const fn1 = (num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          result.push(num++);
+          resolve(num++);
+        }, 500);
+      });
+    };
+    const fn2 = (num) => {
+      return new Promise((_, reject) => {
+        setTimeout(() => {
+          result.push(num++);
+          reject("fn2");
+        }, 100);
+      });
+    };
+    const composeFn = syncCompose([fn1, fn2]);
+    try {
+      await composeFn(1);
+    } catch (error) {
+      expect(error === "fn2");
+      expect(result).toEqual([1, 2]);
+    }
+  });
 });
